937b536cfbf067104c2b306f8a6a3d0bc889fe46
Vannio <singularity@vannio.co.uk>
2016-01-30 00:23:15 +0000

    Added contact button to nav

--- _js/main.js
+++ _js/main.js
@@ -16,11 +16,28 @@ function ajaxLoad(target){
         $(outer).html(content);
         $(outer).hide().fadeIn('fast');
       });
-    }
+    },
+    error: function(){
+      $(outer).fadeOut('fast',function(){
+        $(outer).html('');
+        $(outer).html('<div class="text-wrapper">Oops, something went wrong!</div>');
+        $(outer).hide().fadeIn('fast');
+      });
+    }
   });
 };
 
-$('body').on('click','.site-nav a, .post-title a', function(e){
+$('body').on('click','.ajax-link', function(e){
   e.preventDefault();
   ajaxLoad($(this).attr('href'));
+});
+
+// Smooth scrolling to contact anchor
+$('body').on('click','.contact-link',function(e) {
+  e.preventDefault();
+
+  var target = $(this.hash);
+  $('html,body').animate({
+    scrollTop: target.offset().top
+  }, 1000);
 });
