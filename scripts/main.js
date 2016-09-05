d048192f0f07aaad73aadc48e678bfb049d68e9d
Van <singularity@vannio.co.uk>
2016-09-05 01:25:38 +0100

    Update 2016-09-05T00:25:36.586Z

--- scripts/main.js
+++ scripts/main.js
@@ -48,17 +48,41 @@
 
 	// Disable the mobile nav button from doing anything!
 	document.getElementById('js-menu').addEventListener('click', function (e) {
-		e.preventDefault();
+	  e.preventDefault();
 	});
 
 	// Expanding project challenges
 	var challenges = document.getElementsByClassName('js-challenges');
 	for (var i = 0; i < challenges.length; i++) {
-		challenges[i].addEventListener('click', function (e) {
-			if (e.target.nodeName !== 'A') {
-				this.classList.toggle('open');
-			}
-		});
+	  challenges[i].addEventListener('click', function (e) {
+	    if (e.target.nodeName !== 'A') {
+	      this.classList.toggle('open');
+
+	      // Send click event to Google Analytics when opened
+	      var projectName = getProjectName(e.target);
+	      if (projectName) {
+	        ga('send', 'event', 'project', 'toggleVisibility', projectName);
+	      }
+	    }
+	  });
+	}
+
+	// -----------------
+	// Helper functions!
+	function getProjectName(el) {
+	  var regexpClassNames = /js-challenges.*open/;
+
+	  if (el.className.match(regexpClassNames)) {
+	    return getLinkText(el);
+	  } else if (el.parentNode.className.match(regexpClassNames)) {
+	    return getLinkText(el.parentNode);
+	  } else {
+	    return false;
+	  }
+	}
+
+	function getLinkText(el) {
+	  return el.getElementsByTagName('a')[0].innerText;
 	}
 
 /***/ }
